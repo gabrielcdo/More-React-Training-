0c636fd7fc3191e6feb439f99bd737a018e1cace
gabrielcdo <gabrielgc.883@gmail.com>
2021-03-22 21:40:41 -0300

    com parametros

---
+++ src/components/Parameter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Parameter( props ){
+
+    return(
+        <div>
+        <h3>{props.titulo}</h3>
+        <p>{props.subtitulo}</p>
+        </div>
+    )
+}
+export default Parameter;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import First from './components/First';
 import Hello from './components/Hello'
+import Parameter from './components/Parameter';
 import './index.css'
 
 
 ReactDOM.render(
   <div>
-    <Hello></Hello>
+    
     <First/>
+    <Parameter titulo= " Este é o Titulo " subtitulo = " Este é o Subtitulo "></Parameter>
     </div>
   ,
   document.getElementById('root')
