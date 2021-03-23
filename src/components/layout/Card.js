bea686afe49c94e1cb56ff57a69d073a244538bf
gabrielcdo <gabrielgc.883@gmail.com>
2021-03-22 22:14:57 -0300

    Card

---
+++ src/components/layout/Card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Card.css'
+function Card(props){
+    
+    return (
+    <div className="Card">
+        <div className="Conteudo">{props.children}</div>
+        <div className="Footer">{props.titulo}</div>
+    </div>
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import First from './components/First';
 import Hello from './components/Hello'
+import Card from './components/layout/Card';
 import Parameter from './components/Parameter';
 import Sons from './components/Sons';
 import './index.css'
@@ -9,6 +10,11 @@ import './index.css'
 
 ReactDOM.render(
   <div>
+    <Card titulo= "Exercício X">
+      Conteudo
+    </Card>
+    
+    {/*
     <Sons>
       <ul>
         <li>hello</li>
@@ -17,7 +23,7 @@ ReactDOM.render(
         <li>Gabriel</li>
       </ul>
     </Sons>
-    {/*<First/>
+    <First/>
     <Parameter titulo= " Este é o Titulo " subtitulo = " Este é o Subtitulo "></Parameter>
     <Parameter titulo= " Opa " subtitulo = "Epa "></Parameter>*/}
     </div>
