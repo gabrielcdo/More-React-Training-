e80b90a444fb97a1ac8e7ae2b2eb1bb486001421
gabrielcdo <gabrielgc.883@gmail.com>
2021-03-22 21:29:38 -0300

    first commit

---
+++ src/components/First.js
@@ -0,0 +1,10 @@
+function First(){
+
+    return (
+        <div>
+            <h1>First</h1>
+            <h2>Second</h2>
+        </div>
+    )
+}
+export default First;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import First from './components/First';
+import Hello from './components/Hello'
+import './index.css'
+
+
+ReactDOM.render(
+  <div>
+    <Hello></Hello>
+    <First/>
+    </div>
+  ,
+  document.getElementById('root')
+);
+
+
